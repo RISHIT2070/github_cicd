ea8a4e1bee423da3ec1ab24ced6f2392ab1409a3
RISHIT2070 <rishitsuman1@gmail.com>
2024-07-24 12:16:22 +0530

    added

--- main.tf
+++ main.tf
@@ -6,8 +6,5 @@ resource "aws_s3_bucket" "example" {
   bucket_prefix = "infrasity-bucket" # Bucket name prefix
   force_destroy = true               # Allow Terraform to delete non-empty buckets
 
-  tags = {
-    Name        = "infrasity-bucket"
-    Environment = var.environment
-  }
+
 }
